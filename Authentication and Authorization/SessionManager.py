9ffb09c46d1c4efb09b77c16f6b458b41d594b77
ailishc8-byte <ailishc8@gmail.com>
2026-02-17 15:54:18 -0800

    Add SessionManager class

---
+++ Authentication and Authorization/SessionManager.py
@@ -0,0 +1,11 @@
+class SessionManager: 
+
+    def create_session(self, user_id: str) -> str: 
+        return " "; 
+
+    def validate_session(self, token: str) -> bool:
+        return True; 
+
+    def expire_session(self, token: str) -> None:
+        pass
+
