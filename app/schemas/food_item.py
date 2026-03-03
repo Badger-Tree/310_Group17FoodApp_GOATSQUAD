5bb1b3f91201a9232345437e67e6c2853225b37f
megan <davamae.md@gmail.com>
2026-03-03 10:40:22 -0500

    tidied up schema for food_item.py

--- app/schemas/food_item.py
+++ app/schemas/food_item.py
@@ -6,7 +6,7 @@ from decimal import Decimal
 class FoodItemBase(BaseModel):
     food_name: str
     restaurant_id: int
-    price: Decimal = Field(..., max_digits=10, decimal_places=2, examples=[12.99])
+    price: Decimal = Field(max_digits=10, decimal_places=2)
     description: str
     course: str
 
@@ -14,13 +14,11 @@ class FoodItemCreate(FoodItemBase):
     pass #inherits from FoodItemBase; adds nothing new
 
 class FoodItemUpdate(BaseModel):
-    #every field = optional so user can update price w/o re-sending name
     food_name: Optional[str] = None
     price: Optional[Decimal] = None
     description: Optional[str] = None
     course: Optional[str] = None
 
-#what api sends back to user - the Response
 class FoodItem(FoodItemBase):
     food_item_id: int
 
