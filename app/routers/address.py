c3c3ba26b8548511a0ee32156542ac53445cc76c
Badger-Tree <colleenhaukaas@gmail.com>
2026-03-07 13:56:46 -0800

    added input validation to address schema

--- app/routers/address.py
+++ app/routers/address.py
@@ -8,21 +8,26 @@ router = APIRouter(prefix="/addresses", tags=["addresses"])
 
 @router.get("/by-id/{address_id}", response_model=AddressResponse)
 def get_address_by_id(address_id: str):
+    """function gets an address object (AddressResponse) given an address id"""
     return get_address_by_id_service(address_id)
 
 @router.get("/by-customer/{customer_id}", response_model=List[AddressResponse])
 def  get_address_by_customer_id(customer_id: str):
+    """function gets a list of address objects (AddressResponse) given a customer id"""
     return get_address_by_customer_id_service(customer_id)
 
 @router.post("", response_model=AddressResponse, status_code=201)
-def create_address(payload: AddressCreate):
-    return create_address_service(payload)
+def create_address(payload: AddressCreate, userid: str):
+    """function creates an address given an AddressCreate payload"""
+    return create_address_service(payload, userid)
 
 @router.put("/update/{addressid}", response_model=AddressResponse)
 def update_address(addressid: str, payload: AddressUpdate):
+    """function updates an address given an AddressUpdate payload"""
     return update_address_service(addressid, payload)
 
 @router.delete("/delete/{addressid}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_address(addressid:str):
+    """function deletes an address object given an address id"""
     delete_address_service(addressid)
     return None
---
+++ test/test_schemas/test_Address.py
@@ -0,0 +1,12 @@
+from pydantic import ValidationError
+from datetime import datetime
+import pytest
+from app.schemas.Address import Address, AddressCreate, AddressUpdate, AddressResponse
+
+# def test_Address_success():
+
+
+
+# def test_AddressCreate_success()
+# def test_AddressUpdate_success()
+# def test_AddressResponse_success()
