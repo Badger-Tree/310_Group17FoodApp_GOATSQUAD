5ddc89f7b3a96d1fbe5b8d7ebec67fac8dcb151e
Badger-Tree <colleenhaukaas@gmail.com>
2026-03-08 21:30:19 -0700

    structure and data for auth

--- app/routers/authentication.py
+++ app/routers/authentication.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, status
 from app.schemas.Login import LoginRequest, LoginResponse
-from app.services.authentication_service import login_service, get_user_from_token_service
-router = APIRouter(prefix="/login", tags=["login"])
+from app.services.authentication_service import login_service, get_user_from_token_service, logout_service
+router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.post("/login", response_model=LoginResponse)
 async def login(credentials: LoginRequest):
     return login_service(credentials)
     
 def helper_get_user_from_token(token:str):
-    get_user_from_token_service(token)
+    return get_user_from_token_service(token)
+
+@router.post("/logout")
+def logout(token:str):
+    return logout_service(token)
--- app/services/authentication_service.py
+++ app/services/authentication_service.py
@@ -5,6 +5,7 @@ from app.repositories.users_repo_csv import load_all as load_users
 import secrets
 from app.schemas.Login import LoginRequest, LoginResponse, TokenResponse
 from app.services.user_service import get_user_by_email_service, get_user_by_id_service
+from app.repositories.sessions_repo import load_all as load_sessions, save_all as save_sessions
 
 def login_service(credentials: LoginRequest) -> LoginResponse:
     user = get_user_by_email_service(credentials.email)
@@ -12,24 +13,42 @@ def login_service(credentials: LoginRequest) -> LoginResponse:
         raise HTTPException(status_code=401, detail=f"Incorrect credentials")
     token = create_token(user.get("id"))
     return LoginResponse(token = token, user_id=user.get("id"),user_role=user.get("role"))
-    
-
-sessions = {}
 
 def create_token(userid: str) -> str:
     token = secrets.token_hex(16)
-    created = datetime.utcnow()
+    created = datetime.now(datetime.timezone.utc)
     expires = created + timedelta(hours=1)
-    # add session to sessions csv
-    
+
+    sessions = load_sessions()
+    sessions.append({"userid":userid,
+                     "token": token,
+                     "created" : created.isoformat(),
+                     "expires" : expires.isoformat()})
+    save_sessions(sessions)
     return token
 
 def get_user_from_token_service(token: str) -> TokenResponse:
-    userid = sessions.get(token)
-    if not userid:
-        raise HTTPException(status_code=401, detail="Invalid token")
-    user = get_user_by_id_service(userid)
-    return TokenResponse(token=token,
+    sessions = load_sessions()
+    for session in sessions:
+        if session["token"] == token:
+            if datetime.now(datetime.timezone.utc) > session["expires"]:
+                raise HTTPException(status_code=401, detail="session expired")
+            user = get_user_by_id_service(session.get("userid"))
+            return TokenResponse(token=token,
                          email=user.get("email"), 
                          user_id = user.get("id"), 
-                         role = user.get("role"))
+                         role = user.get("role")
+            )
+    raise HTTPException(status_code=401, detail="invalid token")
+
+def logout_service(token:str):
+    sessions = load_sessions()
+    new_sessions = []
+    for session in sessions:
+        if session["token"] != token:
+            new_sessions.append(session)
+        if len(new_sessions) == sessions:
+            #this means nothing was deleted
+            raise HTTPException(status_code=401, detail="invalid token")
+    save_sessions(new_sessions)
+    return {"detail" : "logout successful"}
