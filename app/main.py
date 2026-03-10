152beb7bb0ee91233251cc5db1daaac60e7e8525
ailishc8-byte <ailishc8@gmail.com>
2026-03-09 21:03:43 -0700

    Saving current work before switching to feature/get_cart_item_by_id

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from app.routers.cartItems import router as cartItems_router
-from app.routers.cart import router as cart_router
 from .routers.food_item import router as food_router
 
 from app.routers.user import router as user_router
@@ -18,7 +17,6 @@ def root():
     return {"message": "FastAPI is running!"}
 
 app.include_router(cartItems_router)
-app.include_router(cart_router)
 
 app.include_router(user_router)
 app.include_router(address_router)
