8d2c45eb3aa09215811bdb769182e7d76a665097
spliesh <hiteshtejwanischool@gmail.com>
2026-03-11 11:58:41 -0700

    Fixed conflicts locally and tested code
    Fixed merge conflicts locally. These conflicts were in __pyache__ and required to be resolved locally. Why running tests, I had to remove test_session_manager.py file since it was incomplete and caaused errors while running pytest. Inside the test_session_manager, it only had three lines of code with import. All test passed and the merge seems to be okay.

---
+++ app/schemas/Restaurant.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from datetime import time
+from typing import Optional
+
+
+""" This is the base model for restaurant"""
+class RestaurantBase(BaseModel):
+    restaurant_name: str
+    cuisine: str
+    address: str
+    open_hour: time
+    closed_hour: time
+
+"""User creates a restaurant with this"""
+class RestaurantCreate(RestaurantBase):
+    pass
+
+"""User can update any of the restaurant information except for the ids"""
+class RestaurantUpdate(BaseModel):
+    restaurant_name: Optional[str] = None
+    cuisine: Optional[str] = None
+    address: Optional[str] = None
+    open_hour: Optional[time] = None
+    closed_hour: Optional[time] = None
+    restaurant_status: Optional[str] = None
+
+"""When a user creates a restaurant, the system will return with a restaurant id, the owner id given earlier    
+    and the restaurant status"""
+class RestaurantResponse(RestaurantBase):
+    restaurant_id: int
+    owner_id: str
+    restaurant_status: str
