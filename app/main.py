f3c91084ff3e35c70df33cea6674894133224a59
ailishc8-byte <ailishc8@gmail.com>
2026-03-11 19:43:32 -0700

    Trying to resolve final conflicts

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from app.routers.cartItems import router as cartItems_router
-from .routers.food_item import router as food_router
-from app.routers.cart import router as cart_router
 from app.routers.food_item import router as food_router
 from app.routers.user import router as user_router
 from app.routers.address import router as address_router
-from app.routers.inventory_router import router as inventory_router
+from app.routers.authentication import router as authentication
 
 app = FastAPI()
 
@@ -18,11 +16,10 @@ def root():
     return {"message": "FastAPI is running!"}
 
 app.include_router(cartItems_router)
-
 app.include_router(user_router)
 app.include_router(address_router)
 app.include_router(food_router)
 app.include_router(authentication)
-app.include_router(cart_router)
-app.include_router(inventory_router)
+
+
 
