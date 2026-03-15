c480945997d43b5c1020f005a26e7a198e7f331a
Badger-Tree <colleenhaukaas@gmail.com>
2026-03-14 17:24:41 -0700

    added services and tests to get order by user id, restaurant id, order id

---
+++ app/services/order_service.py
@@ -0,0 +1,56 @@
+import random
+from typing import List
+from fastapi import HTTPException
+from datetime import datetime, timezone
+from app.repositories.orders_repo import load_all as load_orders, save_all as save_all_orders
+from app.repositories.order_items_repo import load_all as load_order_items, save_all as save_all_order_items
+from app.schemas.Order import OrderResponse, OrderCreate
+from app.schemas.OrderItem import OrderItemResponse # type: ignore
+from app.schemas.OrderStatus import OrderStatus
+import uuid
+from enum import Enum
+
+
+def get_order_by_order_id_service(orderid:str)-> OrderResponse | None:
+    """Method gets a single OrderResponse object. Takes in an order id (str)"""
+    order_data = load_orders()
+    order_item_data = load_order_items()
+    
+    for order in order_data:
+        if order.get("order_id") == orderid:
+            items_response = []
+            for item in order_item_data:
+                if item.get("order_id") == orderid:
+                    items_response.append(OrderItemResponse(**item))
+            return OrderResponse(**order, items=items_response)
+    return None
+
+def get_orders_by_restaurant_service(restaurantid:int)-> List[OrderResponse]:
+    """Method gets list of Order Response objects matching to a restaurant id. Takes in restaurant id"""
+    order_data = load_orders()
+    order_item_data = load_order_items()
+    
+    order_responses = []
+    for order in order_data:
+        if order.get("restaurant_id") == restaurantid:
+            items_responses = []
+            for item in order_item_data:
+                if item.get("order_id") == order.get("order_id"):
+                    items_responses.append(OrderItemResponse(**item))
+            order_responses.append(OrderResponse(**order, items = items_responses))
+    return order_responses
+
+def get_orders_by_userid_service(userid:str)-> List[OrderResponse]:
+    """Method gets list of OrderResponse objects. Takes in userid (str)"""
+    order_data = load_orders()
+    order_item_data = load_order_items()
+    
+    order_responses = []
+    for order in order_data:
+        if order.get("customer_id") == userid:
+            items_responses = []
+            for item in order_item_data:
+                if item.get("order_id") == order.get("order_id"):
+                    items_responses.append(OrderItemResponse(**item))
+            order_responses.append(OrderResponse(**order, items = items_responses))
+    return order_responses
