b45f8b54889a797fdbd1b75ceec06d868ad0d161
megan <davamae.md@gmail.com>
2026-03-18 19:29:26 -0700

    deleted notif routers and tests, no longer needed

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ from app.routers.user import router as user_router
 from app.routers.address import router as address_router
 from app.routers.inventory_router import router as inventory_router
 from app.routers.order import router as order_router
-from app.routers.notification_router import router as notification_router
 
 app = FastAPI()
 
@@ -25,5 +24,4 @@ app.include_router(address_router)
 app.include_router(food_router)
 app.include_router(inventory_router)
 app.include_router(order_router)
-app.include_router(notification_router)
 
