fe0d931c713ddf8a0d79e0fa0ee81390720c62a4
ailishc8-byte <ailishc8@gmail.com>
2026-03-23 18:05:18 -0700

    Made changes to cart_add so items are not overwritten in cart. Also made changes so that tests pass.

--- test/test_repositories/test_cart_items_repo_csv.py
+++ test/test_repositories/test_cart_items_repo_csv.py
@@ -1,32 +1,45 @@
 from unittest.mock import patch
 import app.repositories.cart_items_repo_csv as repo
+from app.schemas.cart_schema import CartResponse
 import pytest
 
-def test_load_all_with_valid(mocker):
+def test_load_all_with_valid(mocker, tmp_path):
     """Creates valid load all mock data and ensures the values equal what they should"""
+    mock_path = tmp_path / "cartItems.csv"
+    
     mock_data = ("cart_item_id,food_item_id,price_per_item,quantity,subtotal\n"
             "FHDSJFKSJF,1,6.0,1,6.0")
 
-    with mocker.patch("app.repositories.cart_items_repo_csv.Path.exists", return_value=True):
-        mocker.patch("app.repositories.cart_items_repo_csv.Path.open", mocker.mock_open(read_data=mock_data))
+    mock_path.write_text(mock_data)
+    mocker.patch("app.repositories.cart_items_repo_csv.DATA_PATH", mock_path)
+    
     result = repo.load_all()
-    assert result[0]["cart_item_id"] == "FHDSJFKSJF"
+    items = result[0]["cart_item_id"]
+    assert items == "FHDSJFKSJF"
    
 
-def test_load_all_with_empty_file(mocker):
+def test_load_all_with_empty_file(mocker, tmp_path):
     """Creates empty load all mock data and ensures it returns empty data correctly"""
-    with mocker.patch("app.repositories.cart_items_repo_csv.Path.exists", return_value=True):
-        mocker.patch("app.repositories.cart_items_repo_csv.Path.open", mocker.mock_open(read_data=""))
+    mock_path = tmp_path / "cartItems.csv"
+
+    mock_data = ("")
+    
+    mock_path.write_text(mock_data)
+    mocker.patch("app.repositories.cart_items_repo_csv.DATA_PATH", mock_path)
+
     result = repo.load_all()
     assert result == []
 
 
-def test_load_all_with_empty_fields(mocker):
+def test_load_all_with_empty_fields(mocker, tmp_path):
     """Creates load all mock data with empty fields and ensures it returns data correctly even when all fields are empty."""
+    mock_path = tmp_path / "cartItems.csv"
     mock_data = ("cart_item_id,food_item_id,price_per_item,quantity,subtotal\n"
             ",,,,")
-    with mocker.patch("app.repositories.cart_items_repo_csv.Path.exists", return_value=True):
-        mocker.patch("app.repositories.cart_items_repo_csv.Path.open", mocker.mock_open(read_data=mock_data))
+    
+    mock_path.write_text(mock_data)
+    mocker.patch("app.repositories.cart_items_repo_csv.DATA_PATH", mock_path)
+    
     result = repo.load_all()
 
     assert result[0]["cart_item_id"] == ""
@@ -65,7 +78,9 @@ def test_save_all_invalid_format_data(tmp_path):
     
     repo.DATA_PATH = mock_path
     
-    invalid_data = {"cart_item_id": "FHSJFKSJFGKA"}
+    invalid_data = {
+        "cart_item_id": "FHSJFKSJFGKA"
+        }
  
     with pytest.raises(ValueError, match = "Data should be a list"):
         repo.save_all(invalid_data)
