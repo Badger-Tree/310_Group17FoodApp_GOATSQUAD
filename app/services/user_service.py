63030b5606415da50164d69aed25759319b7fbba
Badger-Tree <colleenhaukaas@gmail.com>
2026-04-06 19:35:40 -0700

    added router, service, and tests to get a list of all registered users

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,10 +1,15 @@
 from typing import List
 from fastapi import HTTPException
-from app.repositories.users_repo_csv import find_by_email_repo, find_by_id_repo, find_by_email_repo, add_user_repo, update_user_repo
+from app.repositories.users_repo_csv import find_by_email_repo, find_by_id_repo, find_by_email_repo, add_user_repo, update_user_repo, load_all as load_users
 from app.schemas.User import UserResponse, UserUpdate
 from app.factories.user_factory import CustomerFactory, StaffFactory, CustomerCreate, StaffCreate
 from app.schemas.Role import UserRole
 from app.services.cart_service import create_cart
+
+def get_users_service() -> List[UserResponse]:
+    """returns a list of all registered users, or empty list, intended for admin interface"""
+    users = load_users()
+    return users
 
 def get_user_by_id_service(userid : str) -> UserResponse:
     """This function returns a UserResponse for a user given a user id"""
